87e3ef4100e37790dc511917a8cf0f979917cca8
4lch4 <git@4lch4.email>
2023-12-06 03:47:01 -0600

    refactor: replaced smokes.sh w/ Apollo util script

---
+++ Apollo.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+APOLLO_ICON_PATH="/home/alcha/Development/Projects/Apollo/Assets/Img/Apollo-Portrait-Square.png"
+
+apollo="/home/alcha/.bun/bin/bun /home/alcha/.nvm/versions/node/v20.10.0/bin/apollo"
+
+$apollo "$@"
